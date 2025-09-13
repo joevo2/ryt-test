5cb766d990116d080bac87692772369ca7288c54
Joel Yek <joevo2@gmail.com>
2025-09-13 18:09:04 +0800

    Added prettier

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,14 +2,19 @@ import { Icon, NativeTabs } from 'expo-router/unstable-native-tabs';
 import React from 'react';
 
 export default function TabLayout() {
-
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
-        <Icon sf={{ default: 'house', selected: 'house.fill' }} drawable="custom_home_drawable" />
+        <Icon
+          sf={{ default: 'house', selected: 'house.fill' }}
+          drawable="custom_home_drawable"
+        />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="explore">
-        <Icon sf={{ default: 'gear.circle', selected: 'gear.circle.fill' }} drawable="custom_home_drawable" />
+        <Icon
+          sf={{ default: 'gear.circle', selected: 'gear.circle.fill' }}
+          drawable="custom_home_drawable"
+        />
       </NativeTabs.Trigger>
     </NativeTabs>
   );
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,28 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*'],
+    // Let Prettier handle quote style. Turn off ESLint's built-in `quotes` and
+    // `jsx-quotes` rules so they don't conflict with Prettier's formatting.
+    // Also explicitly configure the `prettier/prettier` rule to use
+    // single quotes (this ensures the ESLint plugin uses the same setting
+    // even if editor integrations don't pick up .prettierrc for some reason).
+    rules: {
+      quotes: 'off',
+      'jsx-quotes': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'es5',
+        },
+      ],
+    },
   },
 ]);
